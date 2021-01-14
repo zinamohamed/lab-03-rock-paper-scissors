4ec9b828c4facc8194cc8bae65f49a098b0e62b5
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-01-13 16:36:04 -0800

    implementing didUserWin function

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // import functions and grab DOM elements
 import { makeRPSThrow } from './utils.js';
+import { didUserWin } from './utils.js';
 
 const goButton = document.getElementById('guess-botton');
 const currentThrow = document.getElementById('current-game-results');
 const winSpan = document.getElementById('wins');
+const loseSpan = document.getElementById('losses');
+const drawSpan = document.getElementById('draws');
 // initialize state
 
 let wins = 0;
+let losses = 0;
+let draws = 0;
 
 
 // set event listeners to update state and DOM
@@ -15,13 +20,11 @@ goButton.addEventListener('click', () => {
 
   // randomly generate computer throw //
 
-    const computerThrow = Math.floor(Math.random() * 3);
-
-    // function that generates userThrow can be found in utils.js //
+  // function that generates computerThrow can be found in utils.js //
 
   // store computers throw in a const //
 
-    const computerRPSThrow = makeRPSThrow(computerThrow);
+    const computerRPSThrow = makeRPSThrow();
 
   // get user's throw and store that in a const //
 
@@ -29,7 +32,10 @@ goButton.addEventListener('click', () => {
 
     const userThrow = selectedThrow.value;
   
-  // YAY okay now compare the two, and see who wins, loses or if its a draw!! // 
+  // YAY okay now compare the two, and see who wins, loses or if its a draw!! //
+  
+    didUserWin(userThrow, computerRPSThrow); 
+
 
     
 });
--- utils.js
+++ utils.js
@@ -1,16 +1,40 @@
 // create function that genorates computer throw // 
-export function makeRPSThrow(throwNumber) {
-    
+export function makeRPSThrow() {
+
+    const randomNumber = Math.floor(Math.random() * 3);
    
-    if (throwNumber === 1) {
+    if (randomNumber === 1) {
         return 'rock';
     }
-    else if (throwNumber === 2) {
+    else if (randomNumber === 2) {
         return 'paper';
     }
-    else if (throwNumber === 3) {
+    else if (randomNumber === 3) {
         return 'scissors';
     }
 
 }
 
+export function didUserWin(throwOne, throwTwo) {
+    if (throwOne === throwTwo) {
+        return 'draw';
+    }
+    if (throwOne === 'rock' && throwTwo === 'paper') {
+        return 'lose';
+    }
+    if (throwOne === 'paper' && throwTwo === 'scissors') {
+        return 'lose';
+    }
+    if (throwOne === 'scissors' && throwTwo === 'rock') {
+        return 'lose';
+    }
+    if (throwOne === 'rock' && throwTwo === 'scissors') {
+        return 'win';
+    }
+    if (throwOne === 'paper' && throwTwo === 'rock') {
+        return 'win';
+    }
+    if (throwOne === 'scissors' && throwTwo === 'paper') {
+        return 'win';
+    }
+}
